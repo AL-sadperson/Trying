2d3dd7fc2342926e6529050795493047713bf3ef
AL-sadperson <146556614+AL-sadperson@users.noreply.github.com>
2026-03-15 07:04:38 +0300

    Remove showMessage function and related code

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function showMessage() {
-    alert("تم فتح بوابة إلى عالم جديد!");
-}
 
-document.body.classList.add("power-on");
-
-<script>
-const toggleBtn = document.getElementById("crtToggle");
-
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("crt-off");
-
-  if (document.body.classList.contains("crt-off")) {
-    toggleBtn.textContent = "CRT: OFF";
-  } else {
-    toggleBtn.textContent = "CRT: ON";
-  }
